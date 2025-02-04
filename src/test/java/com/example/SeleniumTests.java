b3bb6103de766695d51c328b12bb6d7831a3ea8e
sayom88 <sayom88@yahoo.com>
2025-02-04 18:04:07 +0530

    Update SeleniumTests.java

--- src/test/java/com/example/SeleniumTests.java
+++ src/test/java/com/example/SeleniumTests.java
@@ -1,12 +1,12 @@
 package com.example;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -41,7 +41,6 @@ public class SeleniumTests {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.titleContains("GitHub Actions Selenium"));
 
-        // Assert the search results page title
         Assert.assertTrue(driver.getTitle().contains("GitHub Actions Selenium"), 
                           "Page title did not contain the search query.");
     }
